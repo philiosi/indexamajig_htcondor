a49f2fcd32d65ff3b01eb1b1ed789c1e4fc3cb06
philiosi <philiosnaz@gmail.com>
2023-09-22 14:26:18 +0900

    final version of submit condor job for multiful input files

---
+++ 2_submit_condor_indexing.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+#################################################################################
+#
+# submit jobs of HTcondor for indexing - v2.0
+# This script is developed based on the "2_condor_indexing.sh".
+# It submits condor job(s) with executable "3_exec_indexing.sh".
+#
+# (c) 2023 Gisu Park(PAL-XFEL)			Sang-Ho Na(KISTI)
+# Contact: gspark86@postech.ac.kr		shna@kisti.re.kr
+#
+# 2_condor_indexing.sh				Last Modified Data : 2021/03/09 by Gisu Pakr
+# 2_condor_submit_indexing.sh		Last Modified Data : 2023/09/12 by Sang-Ho Na
+# 2_condor_submit_indexing_v2.sh	Last Modified Data : 2023/09/21 by Sang-Ho Na
+##################################################################################
+DEBUG=0
+
+# Input
+# The directory location is determined based on the input parameter.
+geom_dir="" # Do not assign a value.
+lst_dir="" # Do not assign a value.
+
+# Output
+# 'stream_foler' and 'log' directories are required. Please change directories what you want.
+# Default directory are 'file_stream' and 'log'
+stream_folder="file_stream"
+log="log"
+
+# create folder for output and log
+PROCDIR="$( cd "$( dirname "$0" )" && pwd -P )"
+
+# fourc input type
+# - 1010 : 10 multi lst, multi geom
+# - 1001 : 9  multi lst, single geom
+# - 0110 : 6  single lst, multi geom
+# - 0101 : 5  single lst, single geom
+in_type=0
+
+# asign memory
+MEM=360
+
+if [ ! -d $stream_folder ];then
+		mkdir $stream_folder
+fi
+
+if [ ! -d $log ];then
+		mkdir $log
+fi
+
+# define usage
+usage() {
+		err_msg "Usage: $0 -g abc.geom or geom_dir_name -i mosflm -j 36 -f r0081c00.lst or all -o SASE.stream -p mycell.pdb -e \"--int-radius=3,4,5 --threshold=600 --min-srn=4 --min-gradient=100000\""
+}
+err_msg() { echo "$@" ; } >&2
+err_msg_f() { err_msg "-f option requires lst file or directory, and please check the file exist and readable";}
+err_msg_g() { err_msg "-g option requires geometry file or directory, and please check the file exist and readable";}
+err_msg_i() { err_msg "-i option requires indexing method ex)mosflm, xds. asdf, dirax, xgandalf";}
+err_msg_j() { err_msg "-j option requires number of cpu";}
+err_msg_o() { err_msg "-o option requires stream file name";}
+err_msg_p() { err_msg "-o option requires *.pdb file";}
+err_msg_e() { err_msg "-e option another parameter such as -p, --int-radius, --threshold, --min-srn, --min-fradient ";}
+
+if [ "$#" -lt 10 ]; then
+		usage
+		exit
+fi
+
+# get indexmajig options
+while getopts ":g:i:j:f:e:o:p:" opt; do
+		case $opt in
+			g)
+				g=$OPTARG
+				if [ -d "$g" ]; then # f is direcetory
+					# input type
+					let "in_type=($in_type|2)"	
+					geom_dir=$g
+					if [ $DEBUG -eq 1 ]; then echo "[debug] -g option is directory : mg"; fi
+				elif [ -f "$g" ]; then
+					let "in_type=($in_type|1)"
+					if [ $DEBUG -eq 1 ]; then echo "[debug] -g option is regular file : sg"; fi
+				else
+					err_msg_g
+				fi
+				;;
+			i)
+				i=$OPTARG
+				;;
+			j)
+				j=$OPTARG
+				;;
+			f)
+				f=$OPTARG
+				if [ -d "$f" ]; then # f is direcetory
+					# input type
+					let "in_type=($in_type|8)"
+					lst_dir=$f
+					if [ $DEBUG -eq 1 ]; then echo "[debug] -f option is directory : mf"; fi
+				elif [ -f "$f" ]; then
+					let "in_type=($in_type|4)"
+					if [ $DEBUG -eq 1 ]; then echo "[debug] -f option is regular file : sf"; fi
+				else
+					err_msg_f
+				fi
+				;;
+			e)
+				e=$OPTARG
+				;;
+			o)
+				o=$OPTARG
+				;;
+			p)
+				p=$OPTARG
+				;;
+			:)
+				case $OPTARG in
+					g) err_msg_g ;;
+					i) err_msg_i ;;
+					j) err_msg_j ;;
+					f) err_msg_f ;;
+					e) err_msg_e ;;
+					o) err_msg_o ;;
+					p) err_msg_p ;;
+				esac
+					usage
+					;;
+			\?)
+					err_msg "Invalid option: -$OPTAGR"
+					usage
+					;;
+		esac
+done
+
+if [ $DEBUG -eq 1 ]; then echo "geom_dir is $geom_dir"; fi
+if [ $DEBUG -eq 1 ]; then echo "g is $g"; fi
+if [ $DEBUG -eq 1 ]; then echo "lst_dir is $lst_dir"; fi
+if [ $DEBUG -eq 1 ]; then echo "f is $f"; fi
+
+# job submit function
+job_submit() { 
+	geom=`echo $g | awk -F'.' '{print $1}'`
+	got=$(realpath -m "${PROCDIR}/${geom_dir}/${g}")
+	fot=$(realpath -m "${PROCDIR}/${lst_dir}/${f}")
+	oot=${PROCDIR}/${stream_folder}/${geom}_${i}_${runnum}_${o}
+	pot=${PROCDIR}/${p}
+
+	condor_submit <<-EOF
+	universe = vanilla
+	should_transfer_files = IF_NEEDED
+	output = $log/${geom}_${i}_${runnum}_${streamname}_condor.out
+	error = $log/${geom}_${i}_${runnum}_${streamname}_condor.error
+	log = $log/${geom}_${i}_${runnum}_${streamname}_condor.log
+	request_cpus = ${j}					
+	request_memory = $MEM GB
+	executable = 3_exec_indexing.sh
+	arguments = ${got} ${i} ${j} ${fot} ${oot} ${p} ${e}
+	queue
+	EOF
+
+	echo "indexamajig -g $got --peaks=cxi --inexing=$i -j $j -i $fot -o $oot $e -p $pot"
+}
+
+set_output_naming() {
+	runnum=`echo $f | awk -F'.' '{print $1}'`
+	streamname=`echo $o | awk -F'.' '{print $1}'`
+}
+
+# Analyzing all files in the specific folder.
+case $in_type in
+	10)	if [ $DEBUG -eq 1 ]; then echo "[debug] Input Type $in_type: multiful lst files and multiful geom files"; fi
+		
+		if [ $DEBUG -eq 1 ]; then echo "[debug] start 'while' for reading file list]"; fi 
+		ls "$f"/* | while read file_line
+		do
+			f=$(basename "$file_line")
+			
+			ls "$geom_dir"/* | while read geom_line
+			do
+				g=$(basename "$geom_line")
+				if [ $DEBUG -eq 1 ]; then echo "[debug] submit condor job : $lst_dir/$f and $geom_dir/$g"; fi 
+				set_output_naming
+				job_submit
+			done
+		done
+		;;
+	9)	if [ $DEBUG -eq 1 ]; then echo "[debug] Input Type $in_type: multiful lst files and single geom file"; fi
+		
+		ls "$f"/* | while read file_line
+		do
+			f=$(basename "$file_line")
+			if [ $DEBUG -eq 1 ]; then echo "[debug] submit condor job : $lst_dir/$f and $g"; fi 
+			set_output_naming
+			job_submit
+		done
+		;;
+	6)	if [ $DEBUG -eq 1 ]; then echo "[debug] Input Type $in_type: single lst file and multiful geom files"; fi
+		ls "$geom_dir"/* | while read geom_line
+		do
+			g=$(basename "$geom_line")				
+			if [ $DEBUG -eq 1 ]; then echo "[debug] submit condor job : $f and $geom_dir/$g"; fi 
+			set_output_naming
+			job_submit
+		done
+		;;
+	5)	if [ $DEBUG -eq 1 ]; then echo "[debug] Input Type $in_type: single lst file and single geom file"; fi
+		if [ $DEBUG -eq 1 ]; then echo "[debug] submit condor job : $f and $g"; fi 
+		set_output_naming
+		job_submit
+		;;
+esac 
+
--- 3_exec_indexing.sh
+++ 3_exec_indexing.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 ###############################################################################
 #
-# Indexmajin command line of crystfel - v0.1
+# Indexmajin command line of crystfel - v2.0
+# This script is developed based on the "3_exec_indexing.sh"
+# with 2_condor_submit_indexing.sh".
 #
-# (c) 2021 Gisu Park, PAL-XFEL
-# Contact: gspark86@postech.ac.kr
+# (c) 2023 Gisu Park(PAL-XFEL)		Sang-Ho Na(KISTI)
+# Contact: gspark86@postech.ac.kr	shna@kisti.re.kr
 #
-# Last Modified Data : 2021/03/09
-#
+# 3_exec_indexing.sh  	   Modified Data 2021/03/09 by Gisu Park
+# 		      Last Modified Data 2023/09/12 by Sang-Ho Na
 ###############################################################################
 
 
@@ -21,8 +23,6 @@ source /pal/lib/setup_crystfel-0.9.1_hdf5-1.10.5.sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pal/data/staff/ue_191027_SFX/proc/cheetah/hdf5/test/lib
 
-#echo "time indexamajig -g "${1}"/"${2} "--peaks=cxi --indexing="${3} "-j "${4}  "-i "${1}"/file_list/"${5} "-o "${1}"/file_stream/"${3}_${6}_${7} "-p "${1}"/"${8} ${9}
-
-time indexamajig -g ${1}/${2} --peaks=cxi --indexing=${3} -j ${4}  -i ${1}/file_list/${5}  -o ${1}/file_stream/${3}_${6}_${7} -p ${1}/${8}  ${9} 
+time indexamajig -g ${1} --peaks=cxi --indexing=${2} -j ${3}  -i ${4} -o ${5} -p ${6} ${7}
 
 
