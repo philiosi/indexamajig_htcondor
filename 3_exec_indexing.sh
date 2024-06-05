7c26b9ab114027c156ba35dafdc272b54a0a2efc
philiosi <philiosnaz@gmail.com>
2024-06-05 09:50:23 +0900

    .

--- 3_exec_indexing.sh
+++ 3_exec_indexing.sh
@@ -9,7 +9,7 @@
 # Contact: gspark86@postech.ac.kr	shna@kisti.re.kr
 #
 # 3_exec_indexing.sh  	   Modified Data 2021/03/09 by Gisu Park
-# 		      Last Modified Data 2023/09/12 by Sang-Ho Na
+# 		              Last Modified Data 2024/06/05 by Sang-Ho Na
 ###############################################################################
 
 
@@ -22,6 +22,6 @@ source /pal/lib/setup_crystfel-0.10.1_hdf5-1.10.5.sh
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pal/data/setup/lib:/pal/lib/pal-soft/ccp4-7.0/lib
 #export PATH=$PATH:/pal/lib/pal-soft/ccp4-7.0/bin/:/pal/data/setup/lib:/pal/data/setup/XDS-INTEL64_Linux_x86_64
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pal/data/staff/ue_191027_SFX/proc/cheetah/hdf5/test/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pal/htcondor/lib
 
 time indexamajig -g ${1} --peaks=cxi --indexing=${2} -j ${3}  -i ${4} -o ${5} -p ${6} ${7}
